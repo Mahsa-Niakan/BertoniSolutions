1fcc57a81bba7678f36b384949a9f0d68676d6d0
niakan92_itsu_geeksltd <niakan92.itsu@gmail.com>
2020-08-15 05:57:56 -0500

    First Push (rar file has been uploaded while exam time / there was a problem with VPN so couldnt push directly from VS to Git)

---
+++ wwwroot/js/site.js
@@ -0,0 +1,64 @@
+﻿
+function showComments(pictureId) {
+    console.log(pictureId);
+    $("#comments").html("");
+    $.ajax(
+        "https://jsonplaceholder.typicode.com/comments?postId=" + pictureId,
+
+    ).done(function (result) {
+
+        jQuery.each(result, function (index, value) {
+            console.log(value);
+            let comment = $("#comment-template").clone();
+            comment.find(".comment-name").text(value.name);
+            comment.find(".comment-email").text(value.email);
+            comment.find(".comment-body").text(value.body);
+            comment.appendTo("#comments").show();
+
+        })
+    }
+    )
+}
+$(document).ready(function () {
+    $.ajax(
+        "https://jsonplaceholder.typicode.com/albums",
+
+    ).done(function (result) {
+        console.log(result);
+        jQuery.each(result, function (index, value) {
+            $("<option></option>").text(value.title).val(value.id).appendTo("#album-select");
+        })
+    }
+    )
+
+    $("#select-album-btn").click(
+        function () {
+            $("#pictures-table-body").html("");
+            $.ajax(
+                "https://jsonplaceholder.typicode.com/photos?albumId=" + $("#album-select").val(),
+
+            ).done(function (result) {
+                console.log(result);
+
+                jQuery.each(result, function (index, value) {
+                    $("<tr></tr>").append("<td>" + value.title + "</td>")
+                        .append("<td><img src='" + value.thumbnailUrl + "' /></td>")
+                        .append("<button onclick='showComments(" + value.id + ")'>Show Comments</button>")
+                        .appendTo("#pictures-table-body");
+                })
+            }
+            )
+
+        }
+
+
+
+
+
+
+    )
+
+
+}
+
+)
